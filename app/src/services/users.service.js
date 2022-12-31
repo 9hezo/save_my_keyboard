4ada8c008296003591a69d5be34ea910ae60e8a9
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-01-01 02:26:59 +0900

    feat: 회원가입 비밀번호 암호화 추가

--- app/src/services/users.service.js
+++ app/src/services/users.service.js
@@ -1,5 +1,8 @@
 'use strict';
 
+require('dotenv').config();
+const bcrypt = require('bcrypt');
+
 const UsersRepository = require('../repositories/users.repository');
 const { User } = require('../sequelize/models');
 
@@ -7,22 +10,45 @@ class UsersService {
   usersRepository = new UsersRepository(User);
 
   createUser = async (email, password, name, phone, address, admin, point) => {
-    const findOneUserResult = await this.findOneUser(email);
-    if (findOneUserResult) {
-      return { code: 401, message: '이미 가입된 이메일입니다.' };
-    }
 
-    const createUserResult = await this.usersRepository.createUser(email, password, name, phone, address, admin, point);
-    const response =
-      createUserResult > 0
-        ? { code: 201, message: '회원 가입에 성공하였습니다.' }
-        : { code: 500, message: '회원 가입에 실패하였습니다.' };
-    return response;
+    try {
+      const findOneUserResult = await this.findOneUser(email);
+      if (findOneUserResult) {
+        return { code: 401, message: '이미 가입된 이메일입니다.' };
+      }
+
+      password = await this.encryptPassword(password);
+
+      const createUserResult = await this.usersRepository.createUser(email, password, name, phone, address, admin, point);
+      const response =
+        createUserResult > 0
+          ? { code: 201, message: '회원 가입에 성공하였습니다.' }
+          : { code: 500, message: '회원 가입에 실패하였습니다.' };
+      return response;
+
+    } catch(err) {
+      console.log('users.service error: ', err);
+      return { code: 500, message: '회원 가입에 실패하였습니다.' };
+    }
   };
 
   findOneUser = async (email) => {
     return await this.usersRepository.findOneUser(email);
   };
+
+  encryptPassword = async (password) => {
+    const saltRounds = parseInt(process.env.BCRYPT_SALT);
+    return new Promise((resolve, reject) => {
+      bcrypt.hash(password, saltRounds, (err, hash) => {
+        if (err) {
+          console.log(err);
+          reject(err);
+        } else {
+          resolve(hash);
+        }
+      });
+    });
+  }
 }
 
 module.exports = UsersService;
