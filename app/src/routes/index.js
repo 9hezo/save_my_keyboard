8d4fc3da9825525cb3b0f5fb037822e78f1b01a9
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-01-01 22:13:32 +0900

    feat: 메인-로그인-로그아웃 flow 구현

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -6,4 +6,13 @@ const router = express.Router();
 const usersRouter = require('./users.routes');
 router.use('/users', usersRouter);
 
+router.use('/', (req, res) => {
+  const { accessToken, refreshToken } = req.cookies;
+
+  if (!refreshToken || !accessToken) {
+    return res.render('index');
+  }
+  res.render('index', { userInfo: { name: '테스터', point: 500000 } });
+});
+
 module.exports = router;
