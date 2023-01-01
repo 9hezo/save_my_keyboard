849bb13687b43c80dd7a87ae9da56d2aed6ca4a5
Jo haebeen <haebin1622@naver.com>
2023-01-02 01:13:13 +0900

    v 1.0.1

--- app/src/routes/orders.routes.js
+++ app/src/routes/orders.routes.js
@@ -1,12 +1,17 @@
 'use strict';
 
+const { Router } = require('express');
 const express = require('express');
 const router = express.Router();
+const multer = require("multer");
+const path = require("path");
 
 const OrdersController = require('../controllers/orders.controller');
 const ordersController = new OrdersController();
 
 router.get('/', ordersController.output_orders);
+
+router.get('/', ordersController.getorders);
 
 router.post('/', ordersController.createOrder);
 
