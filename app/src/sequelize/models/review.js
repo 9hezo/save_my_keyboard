5d9baf7ad0e933388cab4cd2b08ff2bf32c49f8c
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-01-02 09:20:20 +0900

    hotfix: DB table 리뉴얼

--- app/src/sequelize/models/review.js
+++ app/src/sequelize/models/review.js
@@ -1,7 +1,7 @@
 'use strict';
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Reviews extends Model {
+  class Review extends Model {
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
@@ -11,7 +11,7 @@ module.exports = (sequelize, DataTypes) => {
       // define association here
     }
   }
-  Reviews.init(
+  Review.init(
     {
       orderId: DataTypes.INTEGER,
       content: DataTypes.TEXT,
@@ -20,8 +20,8 @@ module.exports = (sequelize, DataTypes) => {
     },
     {
       sequelize,
-      modelName: 'Reviews',
+      modelName: 'Review',
     }
   );
-  return Reviews;
+  return Review;
 };
