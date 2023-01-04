742db62882ede873bb5886324f0df56fe5993931
KwonByungSeok <bryan3589@naver.com>
2023-01-04 10:02:19 +0900

    review page front err fixed

--- app/src/routes/index.js
+++ app/src/routes/index.js
@@ -8,6 +8,7 @@ const usersRouter = require('./users.routes');
 const ordersRouter = require('./orders.routes');
 const reviewsRouter = require('./reviews.routes');
 router.use('/users', usersRouter);
+router.use('/reviews', reviewsRouter)
 router.use('/orders', ordersRouter);
 
 router.use('/', authMiddleware, (req, res) => {
