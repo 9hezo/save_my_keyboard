3bbabfe2bd07931a33b17b4f7901dba5fa10100a
Jo haebeen <haebin1622@naver.com>
2023-01-05 23:53:03 +0900

    feat. 윤활 신청 리스트 구현 완료

--- app/src/public/js/orders/orderlists.js
+++ app/src/public/js/orders/orderlists.js
@@ -1,41 +1,41 @@
 'use strict';
-    function statusChange(ownerId) {
+    function statusChange(ownerId) { 
       $.ajax({
-        type: "PUT",
-        url: "/api/orders/status/" + ownerId,
-        data: {},
-        async: false,
-        success: function (response) {
-            window.location.replace = ('/orders/mypage2');
-        },
-        error: function (error) {
-          customAlert(error.responseJSON.errorMessage);
-        }
-      });
-    };
+            type: "PUT",
+            url: "/api/orders/status/" + ownerId,
+            data: {},
+            async: false,
+            success: function (response) {
+                window.location.replace('/users/admin');
+            },
+            error: function (error) {
+              customAlert(error.responseJSON.errorMessage);
+            }
+        });
+    }
 
+      // function statusChange(ownerId) {
+    //   console.log({status_str:1});
+    //   console.log(JSON.stringify({ status_str: 1 }));
+    //   console.log(JSON.parse(JSON.stringify({ status_str: 1 })));
+    //     fetch('/api/orders/status/' + ownerId, {
+    //       method: 'PUT',
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //       },
+    //       body: JSON.stringify({ status: 1 }),
+    //     })
+    //       .then(async (res) => {
+    //         const code = res.status;
 
+    //         res = await res.json();
+    //         alert(res.message);
 
-//   fetch('/api/orders/lists/' + ownerId, {
-//     method: 'PUT',
-//     // headers: {
-//     //   'Content-Type': 'application/json',
-//     // },
-//     body: {
-//       status_str: 1,
-//     },
-//   })
-//     .then(async (res) => {
-//       const code = res.status;
-
-//       res = await res.json();
-//       alert(res.message);
-
-//       if (code === 201) {
-//         location.href = '/orders/mypage2';
-//       }
-//     })
-//     .catch((err) => {
-//       console.log('err: ', err);
-//     });
-// }
+    //         if (code === 201) {
+    //           location.href = '/orders/mypage2';
+    //         }
+    //       })
+    //       .catch((err) => {
+    //         console.log('err: ', err);
+    //       });
+    // }
