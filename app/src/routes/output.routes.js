a89133ad3e7b1553f8eb6ebf7f66da7ddd5e2824
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-01-23 22:44:41 +0900

    refactor: [admin] 키보드 윤활 신청 목록 디자인 리뉴얼 refactor: output page 관련 수정 (ordersRequest -> orders/request 와 같은 자잘한 작명 변경)

--- app/src/routes/output.routes.js
+++ app/src/routes/output.routes.js
@@ -15,8 +15,7 @@ router.get('/login', authMiddleware, usersOutputController.login);
 router.get('/mypage', authMiddleware, usersOutputController.mypage);
 
 router.get('/orders/request', authMiddleware, ordersOutputController.request);
-// 사장님 윤활 신청 목록 페이지
-router.get('/orders/lists', authMiddleware, ordersOutputController.getlists);
+router.get('/orders/list', authMiddleware, ordersOutputController.list);
 
 router.get('/', authMiddleware, (req, res) => {
   res.render('index', {
