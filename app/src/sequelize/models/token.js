257af8583b9cc5ff040ac8e8b556d68de25d02a7
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-01-24 21:16:04 +0900

    refactor: migration 파일 업로드, models associate 정리 refactor: 잘못 지운 reviews/best_review.ejs 파일 복구

--- app/src/sequelize/models/token.js
+++ app/src/sequelize/models/token.js
@@ -2,13 +2,8 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Token extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
-      // define association here
+      this.belongsTo(models.User, { foreignKey: 'userId' });
     }
   }
   Token.init(
--- app/src/sequelize/models/user.js
+++ app/src/sequelize/models/user.js
@@ -2,14 +2,10 @@
 const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
     static associate(models) {
       this.hasMany(models.Order, { foreignKey: 'ownerId' });
       this.hasMany(models.Order, { foreignKey: 'workerId' });
+      this.hasMany(models.Token, { foreignKey: 'userId' });
     }
   }
   User.init(
