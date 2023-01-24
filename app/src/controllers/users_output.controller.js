e0f3af915dce805176a3198dc7d3becaef30008e
cchoseonghun <109499274+cchoseonghun@users.noreply.github.com>
2023-01-24 22:26:51 +0900

    refactor: ejs 진입 시 권한 확인 로직 추가 refactor: output.controller => userInfo 관련 코드 압축

--- app/src/controllers/users_output.controller.js
+++ app/src/controllers/users_output.controller.js
@@ -4,21 +4,21 @@ class UsersOutputController {
   register = (req, res) => {
     res.render('index', {
       components: 'register',
-      userInfo: res.locals.userInfo ? res.locals.userInfo : null,
+      userInfo: res.locals.userInfo ?? null,
     });
   };
 
   login = (req, res) => {
     res.render('index', {
       components: 'login',
-      userInfo: res.locals.userInfo ? res.locals.userInfo : null,
+      userInfo: res.locals.userInfo ?? null,
     });
   };
 
   mypage = (req, res) => {
     res.render('index', {
       components: 'mypage',
-      userInfo: res.locals.userInfo ? res.locals.userInfo : null,
+      userInfo: res.locals.userInfo ?? null,
     });
   };
 }
